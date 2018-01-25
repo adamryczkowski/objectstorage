b0e1074b636c5e209d4b48325b8e16ec0b2682f4
adam <aryczkowski@imgw.pl>
2018-01-25 21:21:27 +0100

    Added few new unit tests

--- tests/testthat/test04-modify.R
+++ tests/testthat/test04-modify.R
@@ -17,7 +17,7 @@ test_that("Updating single object", {
 })
 
 test_that("Updating single object in multi-object archive", {
-  debugonce(double_storage)
+#  debugonce(double_storage)
   storagepath<-double_storage('update_2_obj')
 
   env<-new.env()
@@ -25,7 +25,7 @@ test_that("Updating single object in multi-object archive", {
   #  debugonce(modify_runtime_objects)
   #  debugonce(add_runtime_objects_internal)
   #  debugonce(modify_runtime_archive)
-  debugonce(set_runtime_archive)
+#  debugonce(set_runtime_archive)
   modify_runtime_objects(storagepath = storagepath, obj.environment = env, objects_to_add = 'a', parallel_cpus = 0)
   env2<-new.env()
   load_objects(storagepath, objectnames = 'a', target.environment = env2, flag_double_check_digest = TRUE)
@@ -33,6 +33,37 @@ test_that("Updating single object in multi-object archive", {
 })
 
 test_that("Updating all objects in multi-object archive", {
+  #  debugonce(double_storage)
+  storagepath<-double_storage('update_2_obj_all')
+
+  env<-new.env()
+  env$a<-'bla'
+  env$b<-'blu'
+  #  debugonce(modify_runtime_objects)
+  #  debugonce(add_runtime_objects_internal)
+  #  debugonce(modify_runtime_archive)
+  #  debugonce(set_runtime_archive)
+  modify_runtime_objects(storagepath = storagepath, obj.environment = env, objects_to_add = 'a', parallel_cpus = 0)
+  env2<-new.env()
+  load_objects(storagepath, objectnames = 'a', target.environment = env2, flag_double_check_digest = TRUE)
+  testthat::expect_equivalent(env2, env)
+})
+
+test_that("Updating all objects in multi-object split archive", {
+  #  debugonce(double_storage)
+  storagepath<-double_storage_ex('update_2_obj_ex_all')
+
+  env<-new.env()
+  env$a<-'bla'
+  env$b<-'blu'
+  #  debugonce(modify_runtime_objects)
+  #  debugonce(add_runtime_objects_internal)
+  #  debugonce(modify_runtime_archive)
+  #  debugonce(set_runtime_archive)
+  modify_runtime_objects(storagepath = storagepath, obj.environment = env, objects_to_add = 'a', parallel_cpus = 0)
+  env2<-new.env()
+  load_objects(storagepath, objectnames = 'a', target.environment = env2, flag_double_check_digest = TRUE)
+  testthat::expect_equivalent(env2, env)
 })
 
 
