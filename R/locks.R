97b058d343b03b167e20700f758863f01c8b1601
adam <aryczkowski@imgw.pl>
2018-02-12 10:17:54 +0100

    Better handling lock file names

--- R/locks.R
+++ R/locks.R
@@ -1,10 +1,14 @@
-# Halts execution until the lock is released, or lock expires.
+#' Halts execution until the lock is released, or lock expires.
+#'
+#' @param path Path to the lock file
+#' @param timeout Wall time after which the lock will be considered open regardless if someone opened it
+#' @export
 wait.for.lock<-function(path, timeout=NULL)
 {
   if(is.null(timeout)) {
     timeout<-getOption('objectstorage.default_lock_time')
   }
-  lockfile<-paste0(path,getOption('objectstorage.lock_extension'))
+  lockfile<-get_lock_file(path)
   if (file.exists(lockfile))
   {
     t<-as.numeric(file.mtime(lockfile))
@@ -20,8 +24,13 @@ wait.for.lock<-function(path, timeout=NULL)
   }
 }
 
+#' Tests if the lock is locked
+#'
+#' @param path Path to the lock file
+#' @param timeout Wall time after which the lock will be considered open regardless if someone opened it
+#' @export
 lock.exists<-function(path, timeout=NULL) {
-  lockfile<-paste0(path,getOption('objectstorage.lock_extension'))
+  lockfile<-get_lock_file(path)
   if(is.null(timeout)) {
     timeout<-getOption('objectstorage.default_lock_time')
   }
@@ -43,7 +52,9 @@ lock.exists<-function(path, timeout=NULL) {
 #'
 #' The file will have an extension getOption('objectstorage.lock_extension')
 #'
-#' @param path Location of the lock file
+#' @param path Path to the lock file
+#' @param timeout Wall time after which the lock will be considered open regardless if someone opened it
+#' @export
 create.lock.file<-function(path, timeout=NULL)
 {
   if(is.null(timeout)) {
@@ -51,14 +62,25 @@ create.lock.file<-function(path, timeout=NULL)
   }
   wait.for.lock(path, timeout)
   dir.create(dirname(path), showWarnings = FALSE, recursive = TRUE)
-  write.table(data.frame(), file=paste0(path,getOption('objectstorage.lock_extension')), col.names=FALSE)
+  write.table(data.frame(), file=get_lock_file(path), col.names=FALSE)
 }
 
-# Removes the lock file.
+#' Removes the lock file.
+#' @param path Path to the lock file
+#' @export
 release.lock.file<-function(path)
 {
-  filename<-paste0(path,getOption('objectstorage.lock_extension'))
+  filename<-get_lock_file(path)
   if (file.exists(filename))
     unlink(filename)
 }
 
+
+get_lock_file<-function(path) {
+  ext<-getOption('objectstorage.lock_extension')
+  ext2<-stringr::str_replace(ext, pattern=stringr::fixed('.'), replacement = '\\.')
+  if(!stringr::str_detect(path, stringr::regex(paste0(ext2, '$')))) {
+    path<-paste0(path, ext)
+  }
+  return(path)
+}
