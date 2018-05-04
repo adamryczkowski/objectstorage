7fa9e9a4a955a7323c1a8d51116d5ba5d8d0511e
adam <aryczkowski@imgw.pl>
2018-05-04 10:11:27 +0200

    Added suport for unnamed columns in lists_to_df

--- R/misc.R
+++ R/misc.R
@@ -5,8 +5,16 @@
 #'                     They may contain heterogenous contents or simply be atomic types with length more than 1.
 #' @return Returns a tibble
 #' @export
-lists_to_df<-function(l, list_columns=character(0)) {
-  cns<-names(l[[1]])
+lists_to_df<-function(l, list_columns=character(0), unnamed_default=NA_character_) {
+  classes<-purrr::map(l, class)
+  idx_lists<-which(purrr::map_lgl(classes, ~'list' %in% .))
+  cns<-unique(unlist(purrr::map(l[idx_lists], names)))
+  unnamed_count<-max(purrr::map_int(l, ~length(.)-length(names(.))))
+  if(unnamed_count>0) {
+    unnamed_cns<-paste0('.col_', seq_len(unnamed_count))
+  } else {
+    unnamed_cns<-character(0)
+  }
   nrow<-length(l)
 
   nas<-list()
@@ -27,6 +35,11 @@ lists_to_df<-function(l, list_columns=character(0)) {
     }
     dt[[cn]]<-rep(val, nrow)
   }
+  for(cn in unnamed_cns) {
+    nas[[cn]]<-unnamed_default
+    dt[[cn]]<-rep(unnamed_default, nrow)
+  }
+
   for(i in seq(1, nrow)) {
     for(cn in cns) {
       val<-l[[i]][[cn]]
@@ -37,7 +50,26 @@ lists_to_df<-function(l, list_columns=character(0)) {
         val<-list(list(val))
       } else {
       }
-      set(dt, i, cn, val)
+      tryCatch(
+        set(dt, i, cn, val),
+        error=function(e) {dt[[cn]][[i]]<<-val}
+      )
+    }
+    if(unnamed_count>0) {
+      item<-l[[i]]
+      ucnt<-length(item) - length(names(item))
+      unames<-names(item)
+      if(is.null(unames)) {
+        unames<-rep('', length(item))
+      }
+      un<-1
+      for(j in seq_along(item)) {
+        if(unames[[j]]=='') {
+          val<-item[[j]]
+          set(dt, i, unnamed_cns[[un]], val)
+          un<-un+1
+        }
+      }
     }
   }
   dt[['..delete']]<-NULL
